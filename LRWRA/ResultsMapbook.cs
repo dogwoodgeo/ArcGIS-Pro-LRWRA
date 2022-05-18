97aeaee05ff3b8095270ad024d2fbdee4819d500
Bradley Jones <bjones@dogwoodgeo.com>
2022-05-18 10:11:23 -0500

    Incorporated AI tool into LRWRA tab

---
+++ LRWRA/ResultsMapbook.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArcGIS.Core.CIM;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Catalog;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Core.Geoprocessing;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Extensions;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Mapping;
+using ArcGIS.Desktop.Layouts;
+
+namespace LRWRA_AI
+{
+    internal class ResultsMapbook : Button
+    {
+
+        protected override void OnClick()
+        {
+
+            LayoutProjectItem layout = Project.Current.GetItems<LayoutProjectItem>().FirstOrDefault(item => item.Name.Equals("Layout-Results"));
+
+            if (layout != null)
+            {
+                var toolPath = @"O:\SHARE\405 - INFORMATION SERVICES\GIS_Layers\Python-Tools\Acoustic-Inspections\AI-Mapbooks.tbx\ResultsExport";
+
+                string[] args = new string[0];
+                Geoprocessing.OpenToolDialog(toolPath, args);
+            }
+            else
+            {
+                MessageBox.Show("'Layout-Results' not in currect ArcGIS Pro project. This layout and associated map must be in this project for the 'Results Mapbook' tool to function.", "Layout Missing!");
+            }
+
+
+
+        }
+
+    }
+}
